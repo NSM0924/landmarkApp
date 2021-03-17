9aab2c792609c62227d9e5557712f8f8e1ab0088
NSM0924 <nasungmin50@naver.com>
2021-03-17 22:57:13 +0900

    Add firebase signup

--- Components/SignUpScreen.js
+++ Components/SignUpScreen.js
@@ -1,4 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
+import auth from "@react-native-firebase/auth";
 import React, { useState } from "react";
 import {
   StyleSheet,
@@ -11,7 +12,49 @@ import {
 
 const LogoutScreen: () => React$Node = ({ navigation }) => {
   let secondInput;
-  let thirdInput;
+
+  let [userEmail, setUserEmail] = useState("");
+  let [userPw, setUserPw] = useState("");
+  // console.log(userEmail);
+  // console.log(userPw);
+
+  function signUp() {
+    if (userEmail == "") {
+      alert("이메일을 입력해주세요!");
+    } else if (userPw == "") {
+      alert("비밀번호를 입력해주세요!");
+    } else {
+      auth()
+        .createUserWithEmailAndPassword(userEmail, userPw)
+        .then(() => {
+          alert("성공");
+          console.log("User account created & signed in!");
+          navigation.navigate("Login");
+        })
+        .catch((error) => {
+          if (error.code === "auth/email-already-in-use") {
+            alert("이미 사용 중인 이메일 주소입니다!");
+            console.log("That email address is already in use!");
+          }
+
+          if (error.code === "auth/invalid-email") {
+            alert("이메일 주소의 형식이 잘못되었습니다!");
+            console.log("That email address is already in use!");
+          }
+
+          // if (error.code === "auth/invalid-email") {
+          //   alert("유효하지 않는 이메일 주소입니다!");
+          //   console.log("That email address is invalid!");
+          // }
+
+          if (error.code === "auth/weak-password") {
+            alert("6자 이상의 비밀번호를 입력해주세요!");
+          }
+
+          console.error(error);
+        });
+    }
+  }
 
   return (
     <>
@@ -41,16 +84,8 @@ const LogoutScreen: () => React$Node = ({ navigation }) => {
               returnKeyType="next"
               blurOnSubmit={false}
               onSubmitEditing={() => secondInput.focus()}
-            />
-            <TextInput
-              style={styles.textInput}
-              placeholder="아이디"
-              placeholderTextColor="#808080"
-              autoCapitalize="none"
-              returnKeyType="next"
-              blurOnSubmit={false}
-              ref={(input) => (secondInput = input)}
-              onSubmitEditing={() => thirdInput.focus()}
+              value={userEmail}
+              onChangeText={(text) => setUserEmail(text)}
             />
             <TextInput
               style={styles.textInput}
@@ -59,9 +94,11 @@ const LogoutScreen: () => React$Node = ({ navigation }) => {
               autoCapitalize="none"
               returnKeyType="done"
               secureTextEntry={true}
-              ref={(input) => (thirdInput = input)}
+              ref={(input) => (secondInput = input)}
+              value={userPw}
+              onChangeText={(text) => setUserPw(text)}
             />
-            <TouchableOpacity style={styles.loginBtn}>
+            <TouchableOpacity style={styles.loginBtn} onPress={signUp}>
               <Text
                 style={{ fontSize: 17, fontWeight: "bold", color: "white" }}>
                 회원가입
